429d5e3d0835ffdb6b0e8728c2909adf102700bc
Michael Bestas <mkbestas@lineageos.org>
2025-01-25 20:33:26 +0200

    eqe: Set speaker protection calibration path
    Change-Id: I60f93feeeff74e30d896be50aa7e799ac045f7d5

--- device.mk
+++ device.mk
@@ -139,6 +139,7 @@ TARGET_SCREEN_HEIGHT := 2712
 TARGET_SCREEN_WIDTH := 1220
 
 $(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+$(call soong_config_set,qtiaudio,pal_spkr_protection_path,/mnt/vendor/persist/factory/audio/audio.cal)
 
 # Bluetooth
 PRODUCT_PACKAGES += \
