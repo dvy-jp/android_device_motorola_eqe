790278e7e13304cd4b8b71204dd60471c74e70b9
Dvy <deivyjas@protonmail.com>
2026-07-12 11:16:05 -0300

    eqe: powershare: point HAL to real wireless_boost_en node
    Default LineageOS powershare_path pointed to /sys/class/power_supply/wireless/device/tx_mode,
    which does not exist on eqe. Real reverse-charging node is /sys/class/qcom-battery/wireless_boost_en.
    Also install init rc to grant the HAL (system) write access to the node.

--- device.mk
+++ device.mk
@@ -375,7 +375,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.powershare-service.default
 
-$(call soong_config_set,lineage_powershare,powershare_path,/sys/class/power_supply/wireless/device/tx_mode)
+$(call soong_config_set,lineage_powershare,powershare_path,/sys/class/qcom-battery/wireless_boost_en)
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/init.eqe-powershare.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.eqe-powershare.rc
 
 # Protobuf
 PRODUCT_PACKAGES += \
