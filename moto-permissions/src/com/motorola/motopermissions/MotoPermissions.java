b12ad371359baa0a14578a9803ab8cdbdc413936
Deivy <deivy@localhost>
2026-09-18 13:09:49 +0000

    eqe: declara los permisos de Motorola y le da el grupo camera a Moto Camera (su motor de imagen)

---
+++ moto-permissions/src/com/motorola/motopermissions/MotoPermissions.java
@@ -0,0 +1,7 @@
+package com.motorola.motopermissions;
+
+/** Paquete vacio: solo existe para declarar los permisos de Motorola. */
+public final class MotoPermissions {
+    private MotoPermissions() {
+    }
+}
